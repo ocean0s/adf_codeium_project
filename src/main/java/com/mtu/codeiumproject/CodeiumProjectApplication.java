ab2356c3d24e06d51df87da7ffca7e601b3c7de1
ocean0s <someone@somewhere.notanemail>
2024-11-12 15:36:42 +0000

    Create app, no code has been written outside of automatic initializer.

---
+++ src/main/java/com/mtu/codeiumproject/CodeiumProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mtu.codeiumproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeiumProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeiumProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mtu/codeiumproject/CodeiumProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtu.codeiumproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeiumProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
